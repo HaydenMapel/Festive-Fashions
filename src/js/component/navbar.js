ec81a249d94ac14877a11a2b1c88fb96c120d6e6
puesyo <csanc236@fiu.edu>
2020-11-03 02:23:13 +0000

    working on home view

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/index.scss";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-dark bg-dark mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Logo</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<ul className="navUl">
+					<li>
+						<Link to="/about">
+							<span className="navbar-brand mb-0 h1">home</span>
+						</Link>
+					</li>
+					<li>
+						<Link to="/about">
+							<span className="navbar-brand mb-0 h1">About</span>
+						</Link>
+					</li>
+					<li>
+						<Link to="/about">
+							<span className="navbar-brand mb-0 h1">Shop</span>
+						</Link>
+					</li>
+				</ul>
+				<div>
+					<button>Login</button>
+					<button>
+						<i className="fas fa-shopping-cart" />
+					</button>
+				</div>
 			</div>
 		</nav>
 	);
