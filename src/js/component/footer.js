fa8adb956586e250e8049c36381f808fda13b67b
HaydenMapel <haydenmapel@gmail.com>
2020-11-10 02:02:16 +0000

    Basic routes, about, footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer text-center bg-dark">
+		<div className="text-center text-light">
+			<i className="far fa-copyright" /> T-Shirt Store 2020
+		</div>
 	</footer>
 );
