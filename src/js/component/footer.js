5f486f81bc6251d856042bee78dc0cc9996c7ff3
puesyo <csanc236@fiu.edu>
2020-12-17 02:32:22 +0000

    adding links to social media icons

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -6,28 +6,36 @@ export const Footer = () => {
 	return (
 		<div className="footer container-fluid pt-1">
 			<div className="row">
-				<div className="col-4">
+				<div className="col-5">
 					<div className="row"></div>
 					<div className="mr-3 d-none d-md-inline-block">Follow Us</div>
 					<ul className="d-inline-block p-0">
 						<li className="d-inline-block col-2">
 							<div className="rounded-circle mr-3 socialMediaBadges">
-								<i className="fab fa-facebook-f"></i>
+								<a href="https://www.facebook.com">
+									<i className="fab fa-facebook-f"></i>
+								</a>
 							</div>
 						</li>
 						<li className="d-inline-block col-2">
 							<div className="rounded-circle mr-3 socialMediaBadges">
-								<i className="fab fa-twitter"></i>
+								<a href="https://www.twitter.com">
+									<i className="fab fa-twitter"></i>
+								</a>
 							</div>
 						</li>
 						<li className="d-inline-block col-2">
 							<div className="rounded-circle mr-3 socialMediaBadges ">
-								<i className="fab fa-linkedin-in"></i>
+								<a href="https://www.linkedin.com">
+									<i className="fab fa-linkedin-in"></i>
+								</a>
 							</div>
 						</li>
 						<li className="d-inline-block col-2">
 							<div className="rounded-circle mr-3 socialMediaBadges ">
-								<i className="fab fa-instagram"></i>
+								<a href="https://www.instagram.com">
+									<i className="fab fa-instagram"></i>
+								</a>
 							</div>
 						</li>
 					</ul>
@@ -35,7 +43,7 @@ export const Footer = () => {
 				<div className="col-4 justify-content-center d-flex align-items-center">
 					Copyright &copy; {new Date().getFullYear()} Festive Fashions
 				</div>
-				<div className="col-4 justify-content-end d-flex align-items-center">
+				<div className="col-3 justify-content-end d-flex align-items-center">
 					<Link to="/contact" className="footercontactText">
 						Contact Us
 					</Link>
